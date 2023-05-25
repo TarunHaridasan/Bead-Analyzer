728342cf7dee3826b3688157dd265a7d5381f72c
Tarun Haridasan <tarunharidasan2003@gmail.com>
2023-05-25 15:37:32 -0400

    05 25

--- GUI/convert.bat
+++ GUI/convert.bat
@@ -1,1 +1,2 @@
-C:\Users\Tarun Haridasan\.virtualenvs\Bead_Analyzer-Ve2q_Ib1\Scripts\pyuic5.exe -x UI.ui -o GUI.py
+"C:\Users\Tarun Haridasan\.virtualenvs\Bead_Analyzer-Ve2q_Ib1\Scripts\pyuic5.exe" -x main.ui -o GUI.py
+PAUSE
